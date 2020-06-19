5c68a3c9c158bb999332b526c9c56652ca496bc5
parth-v <parthvashisht20@gmail.com>
2020-06-20 00:58:20 +0530

    updated api

--- server.js
+++ server.js
@@ -6,16 +6,27 @@ app.use(cors());
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public')
+    cb(null, 'uploads');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' +file.originalname )
+    cb(null, Date.now() + '-' + file.originalname );
   }
 });
 
 var upload = multer({ storage }).array('file');
 
 app.post('/upload',function(req, res) {
+  upload(req, res, function (err) {
+	  if (err instanceof multer.MulterError) {
+	      return res.status(500).json(err);
+	  } else if (err) {
+       return res.status(500).json(err);
+    }
+    return res.status(200).send(req.file);
+  });
+});
+
+app.get('/view',function(req, res) {
   upload(req, res, function (err) {
 	  if (err instanceof multer.MulterError) {
 	      return res.status(500).json(err);
