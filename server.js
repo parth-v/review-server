80688884793ea19556c311c31434caf200896d03
parth-v <parthvashisht20@gmail.com>
2020-06-20 23:14:32 +0530

    added more server routes

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var express = require('express');
+const fs = require('fs');
 var app = express();
 var multer = require('multer')
 var cors = require('cors');
 app.use(cors());
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, 'uploads');
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname );
   }
 });
 
 var upload = multer({ storage }).array('file');
 
-app.post('/upload',function(req, res) {
-  upload(req, res, function (err) {
+app.post('/upload',(req, res) => {
+  upload(req, res, (err) => {
 	  if (err instanceof multer.MulterError) {
 	      return res.status(500).json(err);
 	  } else if (err) {
@@ -26,17 +27,22 @@ app.post('/upload',function(req, res) {
   });
 });
 
-app.get('/view',function(req, res) {
-  upload(req, res, function (err) {
-	  if (err instanceof multer.MulterError) {
-	      return res.status(500).json(err);
-	  } else if (err) {
-       return res.status(500).json(err);
-    }
-    return res.status(200).send(req.file);
-  });
+// let hi = async () => {
+  
+// } 
+// hi();
+
+app.get('/view', async (req, res) => {
+  const dir = './uploads';
+  const files = await fs.promises.readdir(dir);
+  return res.json(files);
 });
 
-app.listen(8000, function() {
-    console.log('App running on port 8000');
+app.get('/download', (req, res) => {
+  const dir = './uploads/';
+  const filePath = dir + req.query.name;
+  console.log(filePath);
+  return res.download(filePath);
 });
+
+app.listen(8000, () => console.log('App running on port 8000') );
